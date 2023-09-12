94fb9a116a0aa71eae85f11d7e28a620abdb3eb6
selimojf <selimojf18@gmail.com>
2023-09-12 09:27:45 -0700

    function that prints lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet 
+ *
+ */
+void print_alphabet(void)
+{
+        char al;
+
+        al = 'a';
+        while (al <= 'z')
+        {
+                _putchar(al);
+                 al++;
+        }
+        _putchar('\n');
+}
